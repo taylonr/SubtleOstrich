8dca0e8eaef8f980dd9da58793375d3bb2cad2e6
Nathan Taylor <taylonr@gmail.com>
2013-03-10 18:00:15 -0500

    Better posting from angular

--- SubtleOstrich.Web/Scripts/subtleOstrich/activity.js
+++ SubtleOstrich.Web/Scripts/subtleOstrich/activity.js
@@ -4,16 +4,17 @@
         when('/', { controller: ActivityControl, templateUrl: 'list' }).
         otherwise({ redirectTo: '/' });
   }).factory('Activity', function ($resource) {    
-    var Activity = $resource('/Home/ActivityList', {name: '@Name'});
+    var Activity = $resource('/Home/ActivityList', {name: '@Name', date: '@Date'});
 
     return Activity;
 });
 
 function ActivityControl($scope, Activity) {
+    $scope.date = new Date();
     $scope.activity = Activity.query();
     $scope.save = function() {
         $scope.activity.push({ Name: $scope.name });
-        var act = new Activity({ Name: $scope.name });
+        var act = new Activity({ Name: $scope.name, Date: $scope.date });
         act.$save();
         $scope.name = '';
     };
