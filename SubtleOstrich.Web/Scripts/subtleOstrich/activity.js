630fba7aa4e7e3c1f9e384ea24baeb640b6f52c2
Nathan Taylor <taylonr@gmail.com>
2013-03-16 22:24:59 -0500

    Update sidebars when activity is added/removed

--- SubtleOstrich.Web/Scripts/subtleOstrich/activity.js
+++ SubtleOstrich.Web/Scripts/subtleOstrich/activity.js
@@ -1,9 +1,9 @@
-﻿angular.module('activity', ['ngResource']).
+﻿var activityModule = angular.module('activity', ['ngResource']).
     config(function($routeProvider) {
         $routeProvider.
             when('/', { controller: ActivityControl, templateUrl: 'list' }).
             when('/MonthDashboard', { controller: MonthDashboardController }).
-            when('/YearDashboard', {controller: YearDashboardController}).
+            when('/YearDashboard', { controller: YearDashboardController }).
             otherwise({ redirectTo: '/' });
     }).factory('Activity', function($resource) {
         var Activity = $resource('ActivityList', { name: '@Name', date: '@Date', id: '@Id' });
@@ -15,9 +15,17 @@
         return Dashboard;
     }).factory('YearDashboard', function($resource) {
         return $resource('YearDashboard');
+    }).factory('updateService', function($rootScope) {
+        var updateService = { };
+
+        updateService.broadcastItem = function () {
+            $rootScope.$broadcast('handleBroadcast');
+        };
+
+        return updateService;
     });
 
-function ActivityControl($scope, $location, Activity) {
+function ActivityControl($scope, $location, Activity, updateService) {
     $scope.date = new Date();
     $scope.activity = Activity.query();
 
@@ -25,7 +33,7 @@ function ActivityControl($scope, $location, Activity) {
         var act = new Activity({ Name: $scope.name, Date: $scope.date });
         act.$save(function (a, putResponseHeaders) {
             $scope.activity.push({ Name: a.Name, Id: a.Id });
-            $scope.$emit('listUpdated');
+            updateService.broadcastItem();
         });
         $scope.name = '';
     };
@@ -37,17 +45,29 @@ function ActivityControl($scope, $location, Activity) {
             for (var i = 0; i < $scope.activity.length; i++) {
                 if($scope.activity[i].Id == id) {
                     $scope.activity.splice(i, 1);
-                    $scope.$emit('listUpdated');
+                    updateService.broadcastItem();
                 }
             }
         });
     };
 }
 
-function MonthDashboardController($scope, $location, Dashboard) {
+function MonthDashboardController($scope, Dashboard) {
     $scope.dashboard = Dashboard.get();
+    
+    $scope.$on('handleBroadcast', function (event, args) {
+        $scope.dashboard = Dashboard.get();
+    });
 }
 
-function YearDashboardController($scope, $location, YearDashboard) {
+function YearDashboardController($scope, YearDashboard) {
     $scope.dashboard = YearDashboard.get();
+   
+    $scope.$on('handleBroadcast', function (event, args) {
+        $scope.dashboard = YearDashboard.get();
+    });
 }
+
+ActivityControl.$inject = ['$scope', '$location', 'Activity', 'updateService'];
+MonthDashboardController.$inject = ['$scope', 'Dashboard'];
+YearDashboardController.$inject = ['$scope', 'YearDashboard'];
