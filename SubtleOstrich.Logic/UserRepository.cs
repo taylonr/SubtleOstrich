74f2d267badfd51a4846d4849d936417c1e97e20
Nate Taylor <taylonr@gmail.com>
2014-01-02 18:58:45 -0600

    Handle case where user has not signed up yet.

--- SubtleOstrich.Logic/UserRepository.cs
+++ SubtleOstrich.Logic/UserRepository.cs
@@ -21,7 +21,7 @@ namespace SubtleOstrich.Logic
 
         public User GetBySourceAndId(string providerName, string id)
         {
-            return _mongo.First(u => u.Id == string.Format("{0}:{1}", providerName, id));
+            return _mongo.FirstOrDefault(u => u.Id == string.Format("{0}:{1}", providerName, id));
         }
 
         public IEnumerable<Activity> GetActivities(string id, DateTime date)
